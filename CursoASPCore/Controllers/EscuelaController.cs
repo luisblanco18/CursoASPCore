2935f521b96fb43dc40d55e7a6e921de5375af3b
Luis Blanco <luisblanco1801@gmail.com>
2021-07-03 01:03:21 -0400

    Add Model, Controller and View index of Escuela

---
+++ CursoASPCore/Controllers/EscuelaController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CursoASPCore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CursoASPCore.Controllers
+{
+    public class EscuelaController : Controller
+    {
+        public IActionResult Index()
+        {
+            var escuela = new Escuela();
+            escuela.EscuelaId = Guid.NewGuid().ToString();
+            escuela.Nombre = "Platzi School";
+            escuela.AñoFundacion = 2021;
+            ViewBag.CosaDinamica = "La monja";
+            return View(escuela);
+        }
+    }
+}
--- CursoASPCore/Startup.cs
+++ CursoASPCore/Startup.cs
@@ -50,7 +50,7 @@ namespace CursoASPCore
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Escuela}/{action=Index}/{id?}");
             });
         }
     }
